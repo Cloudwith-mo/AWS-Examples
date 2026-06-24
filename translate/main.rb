4744b574d1615d870ca97971370eb0ff5256be49
Muhammad Adeyemi <muhammadadeyemi20@gmail.com>
2026-06-24 13:44:23 -0500

    translate example

---
+++ translate/main.rb
@@ -0,0 +1,13 @@
+require 'aws-sdk-translate'
+require 'pry'
+
+client = Aws::Translate::Client.new
+
+text = "Hello. This is Muhammad Adeyemi, Utilizng Amazon Tranlsate."
+resp = client.translate_text({
+    text: text,
+    source_language_code: "en",
+    target_language_code: "es"
+})
+
+puts resp.translated_text
